02d3ec4e7c1576c1ac73a1b4678c3bf987545605
Kiwii <2372785546@qq.com>
2026-06-25 08:37:58 +0800

    Delete demo directory
    这个使第一个版本

--- demo/src/main/fatestrangedream/com/example/demo/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
